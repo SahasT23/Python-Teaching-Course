10f56b4af174fa5c41295ee54cdef5fa601f8653
SahasT23 <sahas.st01@gmail.com>
2024-06-25 21:45:06 +0100

    added more points for the task sets

--- 1. Beginner/1.6 Task Set.py
+++ 1. Beginner/1.6 Task Set.py
@@ -1,2 +1,18 @@
 # Will start with the students learning how to write comments 
-# Will provide examples of when comments should or shouldn't be used
+# Will provide examples of when comments should or shouldn't be use
+
+# = operator and significance -> assignment operator
+
+# ==, !=, <=, >=, <, >  need to be explained as comparison operators and where they can be used
+
+# +, -, *, /, //, %, ** use these arithmetic operators and explain them as well as examples 
+
+# =, +=, -=, *=, /=, %=, //=, **=, &=, |=, ^=, >>=, <<= use assignment operators and show their uses
+
+# and, or, not ->  logical operators and uses
+
+# is, is not -> identity operators
+
+# bitwise operators
+
+# Operator order needs to be discussed   
--- 1. Beginner/1.9 Task Set.py
+++ 1. Beginner/1.9 Task Set.py
@@ -1,1 +1,13 @@
 # Still need to decide on this
+
+# If loop example
+
+# Else loop example
+
+# Elif loop example
+
+# If/Else/Elif loop example
+
+# While loops
+
+# For Loops
