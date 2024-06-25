fbfb50835c312d6c35fac71a77e6252591e7255a
SahasT23 <sahas.st01@gmail.com>
2024-06-25 21:50:30 +0100

    added more content to task sets

---
+++ 1. Beginner/1.11 Task Set.py
@@ -0,0 +1,17 @@
+# Lists and processes
+  # creation, indexing and slicing
+  # show examples of this and explain how it works 
+
+# Tuples and processes
+  # creation, indexing and slicing
+  # show examples of this and explain how it works 
+
+# Dictionaries
+  # creation, indexing and slicing
+  # show examples of this and explain how it works 
+
+# Sets
+  # creation, indexing and slicing
+  # show examples of this and explain how it works 
+
+# These examples will be a bit more difficult
